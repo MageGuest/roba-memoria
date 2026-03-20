f8f79bd992f948aeb1940e13be1471c9d3e6bae1
MageGuest <97855661+MageGuest@users.noreply.github.com>
2026-03-20 15:37:25 -0300

    gran cambio

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -7,9 +7,7 @@ const JUMP_VELOCITY = -400.0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
+	var input_direction  = Input.get_vector()
 	# Handle jump.
 
 
