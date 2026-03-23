2d5fb17857a5c9675887330106e200d2c7401ca4
MageGuest <97855661+MageGuest@users.noreply.github.com>
2026-03-23 20:45:56 -0300

    cambios

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -4,12 +4,16 @@ extends CharacterBody2D
 const SPEED = 150.0
 const JUMP_VELOCITY = -400.0
 
+@onready var animatien = $AnimatedSprite2D/AnimationPlayer
 
 func _physics_process(delta):
 	# Add the gravity.
 	var input_direction = Input.get_vector("left", "right", "up", "down")
-	print(input_direction)
 	velocity = input_direction * SPEED
+	if input_direction != Vector2.ZERO:
+		animatien.play("jiggle physics")
+	else:
+		animatien.play("RESET")
 	# Handle jump.
 
 
---
+++ importantes/Game_Manager.gd
@@ -0,0 +1,3 @@
+extends Node
+var MajoRelation
+var MajoFriend
